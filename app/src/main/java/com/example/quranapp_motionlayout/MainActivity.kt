74af4891f028e9eff055c422b17f1dc75ff40965
Maryam mohamed <mm3279@fayoum.edu.eg>
2023-08-05 01:08:11 +0300

    adding index

--- app/src/main/java/com/example/quranapp_motionlayout/MainActivity.kt
+++ app/src/main/java/com/example/quranapp_motionlayout/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.quranapp_motionlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
